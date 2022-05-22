a08aee000c28dcddbdda94e38a3ac776a7b91541
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-23 01:06:42 +0600

    redesign manual card in purchase

--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -12,8 +12,20 @@ const Purchase = () => {
             .then(data => setProduct(data))
     }, [product]);
     return (
-        <div>
-            <h2>Purchase Product: {product.name}</h2>
+        <div className=''>
+            <div className='flex justify-center items-center mt-10'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">{product.name}</h2>
+                    </div>
+                    <figure><img src={product.img} alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
