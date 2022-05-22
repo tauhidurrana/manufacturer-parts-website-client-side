adf0df7616d4dc6c2b5429f8a688609eec4d6e7a
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-22 16:49:06 +0600

    see all products button added

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -4,26 +4,30 @@ import SingleProducts from './SingleProducts';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
-    const sliceProducts = products.slice(0,3);
+    const sliceProducts = products.slice(0, 3);
 
     useEffect(() => {
         fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
 
     return (
         <div>
             <h2 className='text-xl font-bold text-center'>Our Products</h2>
 
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 mb-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 mb-10'>
                 {
-                    sliceProducts.map(product=><SingleProducts
-                    key={product._id}
-                    product = {product}
+                    sliceProducts.map(product => <SingleProducts
+                        key={product._id}
+                        product={product}
                     ></SingleProducts>)
                 }
             </div>
+            <div className='flex justify-center mb-5'>
+                <button class="btn btn-wide">See All Products</button>
+            </div>
+
         </div>
     );
 };
