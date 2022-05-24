378f7d9452bb49295e07ec040cdfcba788f23cb5
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-24 14:34:17 +0600

    only admin can make admin, normal user cant not and error toast addded

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 
-const UserRow = ({user, refetch}) => {
-    const {email, role} = user;
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
 
     const makeAdmin = () => {
         fetch(`http://localhost:5000/user/admin/${email}`, {
             method: 'PUT',
-            headers:{
+            headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res=>res.json())
-        .then(data=>{
-            refetch();
-            toast.success('Made Admin Successfully');
-        })
+            .then(res => {
+                if(res.status === 403){
+                    toast.error('Failed to Make an Admin');
+                }
+               return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success('Made Admin Successfully');
+                }
+            })
 
     }
     return (
-            <tr>
-                <th>1</th>
-                <td>{email}</td>
-                <td>{ role!=='admin' &&<button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
-                <td><button class="btn btn-xs">Remove User</button></td>
-            </tr>
-        
+        <tr>
+            <th>1</th>
+            <td>{email}</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Remove User</button></td>
+        </tr>
+
     );
 };
 
