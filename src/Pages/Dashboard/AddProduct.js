3dffe6f41ee31814920aa0577f90abaea78a6bac
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-25 01:28:21 +0600

    Add a product by admin, success

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/products';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+
+    }
     return (
-        <div>
-            <h2>Add a Product</h2>
+        <div className="card w-96 bg-base-100 shadow-xl ">
+            <div className="card-body">
+                <h2 className="card-title text-2xl justify-center items-center">Add a Product</h2>
+                <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-2 justify-items-center mt-3'>
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Product Name' {...register("name")} />
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Description' {...register("description")} />
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Price' type="number" {...register("price")} />
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Available Quantity' type="number" {...register("availableQuantity")} />
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Min Quantity' type="number" {...register("minQuantity")} />
+                    <input className="input input-bordered w-full max-w-xs" placeholder='Photo URL' {...register("img")} />
+
+                    <input type="submit" value="Add Product" placeholder="Type here" className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary w-full max-w-xs" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/CheckOutForm.js
+++ src/Pages/Dashboard/CheckOutForm.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState } from 'react';
 
 const CheckOutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    // const [cardError, setCardError] = useState('');
+    // const [success, setSuccess] = useState('');
+    // const [processing, setProcessing] = useState(false);
+    // const [transactionId, setTransactionId] = useState('');
+    // const [clientSecret, setClientSecret] = useState('');
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault()
+        if(!stripe || !elements){
+            return 
+        }
+        const card = elements.getElement(CardElement);
+
+        if (card === null) {
+            return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+    }
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
     );
 };
 
