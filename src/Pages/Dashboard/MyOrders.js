dbb7481d3ea3cdd6724cc3dbf45b1f1f482dee2d
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-24 21:05:31 +0600

    payment button and condition added

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,56 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
 
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([])
 
-    useEffect(()=> {
-        if(user){
-            fetch(`http://localhost:5000/order?email=${user.email}`, {
-              method: 'GET',
-              headers:{
-                'authorization':`Bearer ${localStorage.getItem('accessToken')}`
-              }
-            })
-        .then(res=>res.json())
-        .then(data=>{setOrders(data)})
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/order?email=${user.email}`, {
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    },[user])
+      })
+        .then(res => res.json())
+        .then(data => { setOrders(data) })
+    }
+  }, [user])
 
-    return (
-        <div>
-            <h2>My Orders: {orders.length}</h2>
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    {/* <!-- head --> */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Product</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-        { orders.length > 0 &&
-            orders.map((order, index)=> <tr>
-                <th>{index +1}</th>
+  return (
+    <div>
+      <h2>My Orders: {orders.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Payment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.length > 0 &&
+              orders.map((order, index) => <tr>
+                <th>{index + 1}</th>
                 <td>{order.userName}</td>
                 <td>{order.productName}</td>
                 <td>{order.price}</td>
+        <td>
+          {(order.price && !order.paid)&&<Link to={``}><button className="btn btn-xs btn-success">Pay</button></Link>}
+          {(order.price && order.paid)&&<span className="text-success">Paid</span>}
+        </td>
               </tr>)
-        }
-    
-  
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+            }
+
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
