7c4a59734d976de73d2a3f712833bd0dd0558bf9
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-24 15:54:16 +0600

    without admin, user could not see dashboard, added useAdmin hook

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth)
+    const [admin, setAdmin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -18,7 +23,7 @@ const Dashboard = () => {
                     <li><Link to='/dashboard'>My Orders</Link></li>
                     <li><Link to='/dashboard/review'>Add Review</Link></li>
                     <li><Link to='/dashboard/myprofile'>My Profile</Link></li>
-                    <li><Link to='/dashboard/allusers'>All Users</Link></li>
+                    { admin && <li><Link to='/dashboard/allusers'>All Users</Link></li>}
                 </ul>
 
             </div>
